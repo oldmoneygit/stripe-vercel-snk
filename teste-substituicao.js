3bdb1500678d4aa8afa9ef1655e7acc19c95ce9d
oldmoneygit <contacto.oldmoney@gmail.com>
2025-04-12 15:42:49 -0300

    Update teste-substituicao.js

--- teste-substituicao.js
+++ teste-substituicao.js
@@ -1,3 +1,1 @@
-// Teste de substituição de domínio
-const urlAntiga = "https://qxxk00-am.myshopify.com/pages/obrigado";
-console.log("Antigo domínio: qxxk00-am.myshopify.com");
+const lojaAntiga = "https://qxxk00-am.myshopify.com";
